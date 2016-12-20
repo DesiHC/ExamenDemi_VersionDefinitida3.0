9e701740c71af964b8c8bb6a12e02a6ce730bbaa
BlckShazam <Ylevendnc@gmail.com>
2016-12-20 19:19:07 +0100

    Nave
    Crea una nave con físicas que al chocar desaparezca

---
+++ Assets/Scripts/NaveScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NaveScript : MonoBehaviour {
+	private GameControl gcs;
+
+
+	// Use this for initialization
+	void Start () {
+		gcs = GameObject.Find ("GameControl").GetComponent<GameControl> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+		if (col.gameObject.tag == "Asteroide") {
+			gcs.respawn ();
+		}
+	}
+
+}
