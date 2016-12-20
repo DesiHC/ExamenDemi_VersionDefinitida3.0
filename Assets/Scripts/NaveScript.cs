9d1dc34a06088cdf7e1f56aeb3e5cb68e1c72519
BlckShazam <Ylevendnc@gmail.com>
2016-12-20 20:00:12 +0100

    Escenario Robot
    Crea una escena llamada Robot y copia el diseño

--- Assets/Scripts/NaveScript.cs
+++ Assets/Scripts/NaveScript.cs
@@ -5,6 +5,7 @@ public class NaveScript : MonoBehaviour {
 	private GameControl gcs;
 	public float power = 1f;
 	private Rigidbody2D rb;
+	public float giro = 1f;
 
 	// Use this for initialization
 	void Start () {
@@ -17,7 +18,9 @@ public class NaveScript : MonoBehaviour {
 		if (Input.GetKey (KeyCode.Space)) {
 			rb.AddForce (new Vector2(0,power));
 		}
-		
+		//if (Input.GetKey (KeyCode.RightArrow)) {
+		//	rb.AddTorque (float giro);
+		//}
 	}
 
 	void OnCollisionEnter2D(Collision2D col){
@@ -28,3 +31,4 @@ public class NaveScript : MonoBehaviour {
 
 
 }
+	
