9e6103f05df485815ee9b9774880be8880ae426a
BlckShazam <Ylevendnc@gmail.com>
2016-12-20 20:06:15 +0100

    Mivimiento robot 2
    Pequeño arreglo en el movimiento del robot

--- Assets/Scripts/RobotScript.cs
+++ Assets/Scripts/RobotScript.cs
@@ -2,28 +2,29 @@
 using System.Collections;
 
 public class RobotScript : MonoBehaviour {
-	Rigidbody2D rigi;
+	Rigidbody2D rb;
 	public float fuerza = 1;
+	public float velocidad = 5f;
 	private GameControl gcs;
 
 	// Use this for initialization
 	void Start () {
 		
-		rigi = GetComponent<Rigidbody2D>();
+		rb = GetComponent<Rigidbody2D>();
 		gcs = GameObject.Find ("GameControl").GetComponent<GameControl> ();
 	}
 
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey (KeyCode.RightArrow)) {
-			rigi.AddForce (transform.right * fuerza);
-			transform.localScale = new Vector3 (1, 1, 1);
+			rb.velocity = new Vector2(velocidad*fuerza,rb.velocity.y);
+			transform.localScale = new Vector3(1, 1, 1);
 		}
 			
 
 		if (Input.GetKey (KeyCode.LeftArrow)) {
-			rigi.AddForce (transform.right * -fuerza);
-			transform.localScale = new Vector3 (-1, 1, 1);
+			rb.velocity = new Vector2(-velocidad*fuerza,rb.velocity.y);
+			transform.localScale = new Vector3(-1, 1, 1);
 		}
 	}
 
@@ -35,3 +36,4 @@ public class RobotScript : MonoBehaviour {
 		}
 	}
 }
+
