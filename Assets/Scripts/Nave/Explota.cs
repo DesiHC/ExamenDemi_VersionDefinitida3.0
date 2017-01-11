3c29fe637a6756309e9081008882756c2128d7dc
Pedro Marín Ramos <pmarinr@gmail.com>
2017-01-11 18:51:16 +0100

    Segundo ejercicicio
    Nos quedamos casi al final del segundo ejercicio del examen

---
+++ Assets/Scripts/Nave/Explota.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explota : MonoBehaviour {
+	public GameObject plataforma;
+	public GameObject explosion;
+	public Animator panel_animator;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	void OnCollisionEnter2D (Collision2D Col) {
+		panel_animator.SetBool ("Visible", true);
+		Instantiate (explosion, transform.position, transform.rotation); //La minúscula indica que es el Transform del objeto en sí, el objeto donde está este script
+		Destroy (plataforma);
+		Destroy (gameObject); //Lo mismo con gameObject en minúscula. Se refiere al objeto donde está este script.
+	}
+}
---
+++ Assets/Scripts/Nave/NaveScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NaveScript : MonoBehaviour {
+	public float fuerza = 10f;
+	public float giro = 0.5f;
+	Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Space)) {
+			rb.AddForce (transform.up * fuerza);
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			rb.AddTorque (giro);
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			rb.AddTorque (-giro);
+		}
+	}
+}
+	
