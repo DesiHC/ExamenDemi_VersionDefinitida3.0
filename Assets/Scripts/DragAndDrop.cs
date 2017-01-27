28e5e849c5893c4d58e5f5ebf8c730ef64d44378
Desi Hitos <desihitos@gmail.com>
2017-01-27 17:32:17 +0100

    EjercicioClase

---
+++ Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour {
+	Vector3 gameMousePostion;
+	Vector3 editorMousePosition;
+
+	void OnMouseEnter(){
+		Debug.Log ("Entrando");
+	}
+		
+	void OnMouseExit(){
+		Debug.Log ("Saliendo");
+	}
+
+	void OnMouseDown (){
+		Debug.Log ("Pulsado");
+	}
+
+	void OnMouseUp(){
+		Debug.Log ("Fin Pulsado");
+	}
+
+	void OnMouseDrag (){
+		Debug.Log ("Arrastrando");
+		gameMousePostion = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0);
+
+		editorMousePosition = Camera.main.ScreenToWorldPoint (gameMousePostion);
+	
+		transform.position = new Vector3 (editorMousePosition.x, editorMousePosition.y, transform.position.z);
+	}
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/MovimientoRecto.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovimientoRecto : MonoBehaviour {
+	public GameObject plataforma;
+	public Transform inicio;
+	public Transform fin;
+	public float velocidad = 10f;
+	private Vector3 destino; //variable?
+
+	// Use this for initialization
+	void Start () {
+		destino = fin.position; //Fin es un transform, tienes que especificar si quieres position, scale o rotation
+	}
+	
+	// Update is called once per frame
+	void Update () { //Update se ejecuta una vez cada frame
+		plataforma.transform.position = 
+			Vector3.MoveTowards (plataforma.transform.position,
+				destino,
+				velocidad * Time.deltaTime); //MoveTowards mueve algo entre dos puntos. en este caso "plataforma" se mueve desde su posición hasta destino.
+
+		if (plataforma.transform.position == fin.position) {
+			destino = inicio.position;
+		}
+
+		if (plataforma.transform.position == inicio.position) {
+			destino = fin.position;
+		}
+	}
+}
